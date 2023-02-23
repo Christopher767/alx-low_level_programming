a614604d3dc246442f450d05fba039ad4813ea1a
Chris <olayinkachris7@gmail.com>
2023-02-23 14:28:07 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,16 +2,19 @@
 
 /**
  * _isupper - Check if a letter is upper
- * @x: The number to be checked
+ * @c: The number to be checked
  * Return: 1 for upper leteer or 0 for and else
  */
 
-int _isupper(int x)
+int _isupper(int c)
 
 {
-	if (x >= 65 && x <= 90)
+	if (c >= 65 && c <= 90)
 	{
 	return (1);
 	}
+	else
+	{
 	return (0);
+	}
 }
